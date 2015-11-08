61d355fa06eed6cfb5b12e60704a69b97c1b39ad
ydream <ydream17@gmail.com>
2015-11-08 15:54:45 +0800

    m ======

--- server/app.js
+++ server/app.js
@@ -4,9 +4,8 @@ var Path = require('path');
 var serve = require('koa-static-server');
 var app = require('koa')();
 
-// app.use(serve({ rootDir: Path.resolve('../demo/') }))
-app.use(serve({ rootDir: Path.resolve('../demo/'), rootPath: '/demo', index: 'index.html' }))
-app.listen(8080)
+app.use(serve({ rootDir: Path.resolve('../demo/'), rootPath: '/demo', index: 'index.html', log: true, maxage: 31536000000 }));
+app.listen(8080);
 
 console.log('Static Server listening on port 8080.')
 
